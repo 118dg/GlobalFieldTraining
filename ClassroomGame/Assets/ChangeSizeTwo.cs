1ea22835cd4438d9963e9fd53e8ecef96274c394
118dg <118dg@naver.com>
2021-02-15 00:30:01 +0900

    ClassroomGame

---
+++ ClassroomGame/Assets/ChangeSizeTwo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSizeTwo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MakeBigger()
+    {
+        gameObject.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+
+    }
+
+    public void MakeSmaller()
+    {
+        gameObject.transform.localScale = new Vector3(1f, 1f, 1f);
+
+    }
+}
---
+++ ClassroomGame/Assets/Teleport.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+
+    public GameObject player;
+    private int move = 0;
+    private Vector3 movement;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (move-- > 0) {
+            player.transform.position += movement;
+        }
+	}
+
+    public void onTeleportToObject(GameObject targetGameObject) {
+        movement = (new Vector3(
+            targetGameObject.transform.position.x,
+            player.transform.position.y,
+            targetGameObject.transform.position.z + 6) - player.transform.position)/40;
+        move = 40;
+        /*
+        player.transform.position = new Vector3(
+            targetGameObject.transform.position.x-3, 
+            player.transform.position.y, 
+            targetGameObject.transform.position.z+3);
+            */
+    }
+
+}
